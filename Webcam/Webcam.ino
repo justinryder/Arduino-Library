c9b3f71dbbec679abfbaecd1c3a59bb9dcc23cc1
Justin Ryder <justin@justin-ryder.com>
2014-05-31 12:08:20 -0400

    adding webcam - capture picture on button press to sd card, then copy picture to www directory

---
+++ Webcam/Webcam.ino
@@ -0,0 +1,54 @@
+#include <Bridge.h>
+#include <Process.h>
+
+const int buttonPin = 8;
+const String sdPath = "/mnt/sda1/";
+const String picturesPath = "/www/pictures/";
+
+int buttonState = 0;
+
+Process process;
+
+String filename;
+
+void setup()
+{
+  Bridge.begin();
+  
+  pinMode(buttonPin, INPUT);
+}
+
+void loop() 
+{
+  buttonState = digitalRead(buttonPin);
+  
+  if (buttonState == HIGH)
+  {
+    // Generate filename with timestamp
+    filename = "";
+    processShellCommand("date +%s");
+    while (process.available() > 0)
+    {
+      char c = process.read();
+      filename += c;
+    }
+    filename.trim();
+    filename += ".png";
+ 
+    // Take picture
+    processShellCommand("fswebcam " + sdPath + filename + " -r 1280x720");
+    
+    // Copy picture to /www/pictures/
+    processShellCommand("cp " + sdPath + filename + " " + picturesPath);
+    
+    // Upload to Dropbox
+    //process.runShellCommand("python " + sdPath + "upload_process.py " + sdPath + filename);
+    //while(process.running());
+  }
+}
+
+void processShellCommand(String command)
+{
+  process.runShellCommand(command);
+  while(process.running());
+}
