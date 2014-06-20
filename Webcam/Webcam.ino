1191e01359e1f3f7090d67e0a01bb770fd8a2883
Silicon Ghost <siliconghost@gmail.com>
2014-06-20 10:20:37 -0400

    Integrated sensor code
    Added sensors, LED for visual indicator, AC output pin

--- Webcam/Webcam.ino
+++ Webcam/Webcam.ino
@@ -5,12 +5,20 @@
 const String sdPath = "/mnt/sda1/pictures/";
 const String picturesPath = "/www/slideshow/pictures/";
 const String picturesListFile = "/www/slideshow/picturesList.txt";
-const int potPin = 1;    // Analog ping for Potentiometer
-const int sensor1Pin=0;  // Analog pin for sensor 1
-int potValue = 0;  // variable to store the value coming from the potentiometer
+const int potPin1 = A1;    // Analog ping for Potentiometer
+const int potPin2 = A2;
+const int sensor1Pin = A0;  // Analog pin for sensor 1
+const int sensor2Pin = A3;
+const int ledPin= 13;
+const int acPin = 3;  // Digital pin for AC control
+const int remoteACPin = 12; // Digital pin for Remote AC control
+int sensor1Value = 0;
+int sensor2Value = 0;
+int pot1Value = 0;  // variable to store the value coming from the potentiometer
+int pot2Value = 0;
 const bool debugEnabled = false;  // enable debug logging to file
 const String debugLogFile = "/www/slideshow/debuglog.txt";
-const bool picturesEnabled = false; // enable or disable picture taking
+const bool picturesEnabled = true; // enable or disable picture taking
 
 Process process;
 String filename;
@@ -19,36 +27,64 @@ void setup()
 {
   Bridge.begin();
   FileSystem.begin();
+  pinMode(ledPin, OUTPUT);
+  pinMode(acPin, OUTPUT);
+  pinMode(remoteACPin, OUTPUT);
   
 }
 
 void loop() 
 {
-  potValue = analogRead(potPin);  // get current value from pot
-  DebugLog("PotValue: " + String(potValue));
+  pot1Value = analogRead(potPin1);  // get current value from pot
+  DebugLog("Pot1Value: " + String(pot1Value));
+  pot2Value = analogRead(potPin2);  // get current value from pot
+  DebugLog("Pot2Value: " + String(pot2Value));
   
-  int val= analogRead(sensor1Pin);  // get current value from sensor
-  DebugLog("SensorValue: " + String(val));
+  sensor1Value = analogRead(sensor1Pin);  // get current value from sensor
+  DebugLog("Sensor1Value: " + String(sensor1Value));
+  
+  sensor2Value= analogRead(sensor2Pin);  // get current value from sensor
+  DebugLog("Sensor2Value: " + String(sensor2Value));
  
-  if ((val >= potValue) && picturesEnabled)  // trigger action if sensor >= pot
+  if (sensor1Value >= pot1Value || sensor2Value >= pot2Value)  // trigger action if sensor >= pot
   {
-    DebugLog("Taking Picture");
-    generateTimestampFilename();
+    digitalWrite(ledPin, HIGH);
+    digitalWrite(acPin, HIGH);
+    //pressRemoteControl();
+
+    
+    if (picturesEnabled) {
+      DebugLog("Taking Picture");
+      generateTimestampFilename();
+   
+      // Take picture
+      processShellCommand("fswebcam " + sdPath + filename + " -r 1280x720");
+      
+      // Copy picture to /www/pictures/
+      processShellCommand("cp " + sdPath + filename + " " + picturesPath);
+      
+      appendToFile(filename, picturesListFile);
+      
+      // Upload to Dropbox
+      //process.runShellCommand("python " + sdPath + "upload_process.py " + sdPath + filename);
+      //while(process.running());
+     
+    }
+    sensor1Value = 0;
+    sensor2Value = 0;
+  } 
+  else 
+  digitalWrite(ledPin, LOW);
+  digitalWrite(acPin, LOW);
+  //pressRemoteControl();
  
-    // Take picture
-    processShellCommand("fswebcam " + sdPath + filename + " -r 1280x720");
-    
-    // Copy picture to /www/pictures/
-    processShellCommand("cp " + sdPath + filename + " " + picturesPath);
-    
-    appendToFile(filename, picturesListFile);
-    
-    // Upload to Dropbox
-    //process.runShellCommand("python " + sdPath + "upload_process.py " + sdPath + filename);
-    //while(process.running());
-    val = 0;
-  }
- 
+}
+
+void pressRemoteControl()
+{
+  digitalWrite(remoteACPin, HIGH);
+  delay(300);
+  digitalWrite(remoteACPin, LOW);
 }
 
 void processShellCommand(String command)
